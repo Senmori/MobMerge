2035995e4f72d1b91a7380618588a3d0355323dc
Senmori <thesenmori@gmail.com>
2017-12-27 14:36:35 -0500

    Switch to PluginLogger instead of java.util.Logger.

--- src/main/java/net/senmori/mobmerge/MobMerge.java
+++ src/main/java/net/senmori/mobmerge/MobMerge.java
@@ -2,28 +2,34 @@ package net.senmori.mobmerge;
 
 import net.senmori.mobmerge.configuration.ConfigManager;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 public class MobMerge extends JavaPlugin {
-    public static final boolean DEBUG = Boolean.parseBoolean(System.getProperty("mobMergeDebug", "false"));
-    public static final Logger LOG = Logger.getLogger("MobMerge");
+    public static final boolean DEBUG = Boolean.getBoolean("mobMergeDebug");
+    public static PluginLogger LOG;
 
     private FileConfiguration config;
     private ConfigManager configManager;
 
     @Override
     public void onEnable() {
+        LOG = new PluginLogger(this);
         getConfig().options().copyDefaults(true);
         getConfig().options().copyHeader(true);
         saveDefaultConfig();
 
         config = getConfig();
-        configManager = new ConfigManager(config);
+        configManager = new ConfigManager(config, new File(getDataFolder(), "config.yml"));
+
+
     }
 
     @Override
     public void onDisable() {
+        configManager.saveConfig();
     }
 }
